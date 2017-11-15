287ecd818ca13eaa3809a01a4ef8916fd72bdf63
leejunkyu <samie1202@naver.com>
2017-11-15 17:50:12 +0900

    sadsad

---
+++ Step01_comment/src/test/main/MainClass.java
@@ -0,0 +1,19 @@
+package test.main;
+//한줄 주석 입니다.
+/* 
+ 	여러줄 주석입니다.
+ 	1
+ 	2
+ 	3
+ */
+
+public class MainClass { //클래스명 : MainClass
+	
+	//실행순서가 시작되는 특별한 main 메소드
+	public static void main(String[] args) {// 메소드명 : main
+		
+		System.out.println("안녕하세요.");
+		System.out.println("이제시작");
+		System.out.println("main 메소드가 리턴 됩니다.");
+	}
+}
